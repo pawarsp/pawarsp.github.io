b95eb62a1d4d8c4a95e4d26edc26346cdca374ae
Sankalp Pawar <sankalp@posteo.de>
2026-07-19 15:44:52 +0200

    updated astro config, added github workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  // 1. Replace with your actual GitHub domain
+  site: 'https://pawarp.github.io', 
+  
   integrations: [react()]
 });
